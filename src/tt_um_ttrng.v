4da7b6a744b04daf48a5cc982a83dcae417cb438
Steve Jenson <stevej@gmail.com>
2023-07-10 12:28:38 -0700

    add proper wires

--- src/tt_um_ttrng.v
+++ src/tt_um_ttrng.v
@@ -1,10 +1,26 @@
+/*
+ * tt_um_ttrng.v
+ *
+ * SR-latch based TRNG based on [citation needed]
+ *
+ * Author: Steve Jenson
+ */
+
+
+`default_nettype none
+
 module tt_um_ttrng(
-    input wire sel1,
-    input wire sel2,
-    output reg[8] out
+   	input  wire [7:0] ui_in,	// Dedicated inputs
+	output wire [7:0] uo_out,	// Dedicated outputs
+	input  wire [7:0] uio_in,	// IOs: Input path
+	output wire [7:0] uio_out,	// IOs: Output path
+	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
+	input  wire       ena,
+	input  wire       clk,
+	input  wire       rst_n
 );
 
 always @(posedge clk or negedge rst_n)
-    out <= 8'b0000001;
+    uio_out <= 8'b0000001;
 
 endmodule
