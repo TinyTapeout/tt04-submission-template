32463006b43af0555c99164cea37762b35b9b948
Steve Jenson <stevej@gmail.com>
2023-07-11 09:28:55 -0700

    following the best practices guide

--- src/tt_um_ttrng.v
+++ src/tt_um_ttrng.v
@@ -9,15 +9,15 @@
 
 `default_nettype none
 
-module tt_um_ttrng(
+module tt_um_ttrng #( parameter MAX_COUNT = 10_000_000 ) (
    	input  wire [7:0] ui_in,	// Dedicated inputs
 	output wire [7:0] uo_out,	// Dedicated outputs
 	input  wire [7:0] uio_in,	// IOs: Input path
 	output wire [7:0] uio_out,	// IOs: Output path
 	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
-	input  wire       ena,
-	input  wire       clk,
-	input  wire       rst_n
+	input  wire       ena,      // will go high when the design is enabled
+	input  wire       clk,      // clock
+	input  wire       rst_n     // reset_n - low to reset
 );
 
 always @(posedge clk)        
