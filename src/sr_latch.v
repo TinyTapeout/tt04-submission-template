cef1b458a10a2516b0dde67cbc3cbfa8a6bf238e
Steve Jenson <stevej@gmail.com>
2023-08-07 13:59:23 -0700

    fix circular yosys complaint

--- src/sr_latch.v
+++ src/sr_latch.v
@@ -6,18 +6,20 @@ module sr_latch(s, r, q, qn);
 
 // Converted from a nand-gate diagram of an SR latch.
 /* verilator lint_off UNOPTFLAT */
+/*
 wire Q_int, Qn_int;
  
 assign Q_int = ~(s & Qn_int);
 assign Qn_int = ~(r & Q_int);
 assign q = Q_int;
 assign qn = Qn_int;
-/*
+*/
+
 wire a, b;
 nand(a, s, b);
 nand(b, r, a);
 
 assign q = a;
 assign qn = b;
-*/
+
 endmodule
