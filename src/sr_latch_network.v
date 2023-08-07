ef10e89fe001dbc8614a70615586cb97bab41c26
Steve Jenson <stevej@gmail.com>
2023-08-06 21:26:26 -0700

    don't drive a constant

--- src/sr_latch_network.v
+++ src/sr_latch_network.v
@@ -7,7 +7,7 @@ output reg b; // our output bit.
 
 wire [7:0] qs; // = 8'b0000_0000;
 /* verilator lint_off UNUSEDSIGNAL */
-wire [7:0] dummy_qns = 8'b0000_0000; // TODO: these wires are unused, can we elide?
+wire [7:0] dummy_qns; // = 8'b0000_0000; // TODO: these wires are unused, can we elide?
 reg xord_qs = 0;
 wire final_output = 0;
 
